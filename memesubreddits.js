40955783ae4b7cec9d299b110a58de5d67433789
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-27 19:55:33 +0530

    memesubreddits.js

---
+++ memesubreddits.js
@@ -0,0 +1,47 @@
+// src/memesubreddits.js
+
+module.exports = [
+  // General/funny meme subreddits
+  'dankmemes',
+  'memes',
+  'funny',
+  'wholesomememes',
+  'AdviceAnimals',
+  'PrequelMemes',
+  'me_irl',
+  'HolUp',
+  'comedyheaven',
+
+  // Anime meme/image subreddits
+  'animemes',
+  'goodanimemes',
+  'anime_irl',
+  'awwnime',
+  'AnimeFunny',
+  'AnimeMeme',
+  'classicanimemes',
+  'OnePieceMemes',
+  'NarutoMemes',
+  'DBZmemes',
+  'kimetsu_no_yaiba',
+  'Bleachmemes',
+  'jojo_memes',
+  'attackontitanmemes',
+  're_zero',
+  'gurrenlagann',
+  'animecirclejerk',
+  'animememesplus',
+  'LoveLive',
+  'idolmemes',
+  'MobPsycho100',
+  'Kaguya_memes',
+  'Monogatari',
+
+  // Discord meme subreddits
+  'DiscordMemes',
+  'officialdiscordmemes',
+  'discordappmemes',
+  'discord_irl',
+  'discordfunny',
+  'discordmemers'
+];
