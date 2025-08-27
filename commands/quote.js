49e6621fac9cdfcdd88648f46267eaa215354ec3
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-27 22:10:42 +0530

    quote.js

--- commands/quote.js
+++ commands/quote.js
@@ -3,7 +3,6 @@ const quoteManager = require('../utils/quoteManager');
 
 module.exports = {
   name: 'quote',
-
   data: new SlashCommandBuilder()
     .setName('quote')
     .setDescription('Quote related commands')
@@ -11,46 +10,53 @@ module.exports = {
       subcommand
         .setName('setchannel')
         .setDescription('Set the channel for automatic quotes')
-        .addChannelOption(option => option
-          .setName('channel')
-          .setDescription('Channel to post quotes in')
-          .setRequired(true))
+        .addChannelOption(option =>
+          option
+            .setName('channel')
+            .setDescription('Channel to post quotes in')
+            .setRequired(true)
+        )
     )
     .addSubcommand(subcommand =>
       subcommand
         .setName('setinterval')
         .setDescription('Set the interval (in hours) for automatic quotes')
-        .addIntegerOption(option => option
-          .setName('hours')
-          .setDescription('Number of hours (min 1)')
-          .setRequired(true))
+        .addIntegerOption(option =>
+          option
+            .setName('hours')
+            .setDescription('Number of hours (min 1)')
+            .setRequired(true)
+        )
     )
     .addSubcommand(subcommand =>
-      subcommand
-        .setName('get')
-        .setDescription('Get a random anime quote now')
+      subcommand.setName('get').setDescription('Get a random anime quote now')
     ),
 
+  // Slash command handler
   async slashExecute(interaction) {
     const sub = interaction.options.getSubcommand();
-
     if (sub === 'setchannel') {
       if (!interaction.member.permissions.has('ManageGuild')) {
-        return interaction.reply({ content: 'You need Manage Server permission.', ephemeral: true });
+        return interaction.reply({
+          content: 'You need Manage Server permission.',
+          ephemeral: true,
+        });
       }
       await quoteManager.setQuoteChannel(interaction);
-
     } else if (sub === 'setinterval') {
       if (!interaction.member.permissions.has('ManageGuild')) {
-        return interaction.reply({ content: 'You need Manage Server permission.', ephemeral: true });
+        return interaction.reply({
+          content: 'You need Manage Server permission.',
+          ephemeral: true,
+        });
       }
       await quoteManager.setQuoteInterval(interaction);
-
     } else if (sub === 'get') {
       await quoteManager.sendQuote(interaction);
     }
   },
 
+  // Prefix command handler
   async execute(message, client, args) {
     const sub = args[0]?.toLowerCase();
 
@@ -59,18 +65,18 @@ module.exports = {
         return message.reply('You need Manage Server permission to use this.');
       }
       const channel = message.mentions.channels.first();
-      if (!channel) {
-        return message.reply('Please mention a channel.');
-      }
+      if (!channel) return message.reply('Please mention a channel.');
+
       if (!quoteManager.guildConfigs) quoteManager.loadConfig();
+
       if (!quoteManager.guildConfigs[message.guild.id]) {
         quoteManager.guildConfigs[message.guild.id] = {};
       }
+
       quoteManager.guildConfigs[message.guild.id].quoteChannelId = channel.id;
       quoteManager.saveConfig();
       quoteManager.startScheduler(client, message.guild.id);
       return message.reply(`Quote channel set to ${channel.name}`);
-
     } else if (sub === 'setinterval') {
       if (!message.member.permissions.has('ManageGuild')) {
         return message.reply('You need Manage Server permission to use this.');
@@ -79,19 +85,21 @@ module.exports = {
       if (isNaN(num) || num < 1) {
         return message.reply('Please provide a valid number of hours (at least 1).');
       }
+
       if (!quoteManager.guildConfigs) quoteManager.loadConfig();
+
       if (!quoteManager.guildConfigs[message.guild.id]) {
         quoteManager.guildConfigs[message.guild.id] = {};
       }
+
       quoteManager.guildConfigs[message.guild.id].quoteIntervalHours = num;
       quoteManager.saveConfig();
       quoteManager.startScheduler(client, message.guild.id);
       return message.reply(`Quote interval set to every ${num} hour(s)`);
-
     } else if (sub === 'get') {
       await quoteManager.sendQuote(message);
-
     } else {
+      // Default to sending a random quote
       await quoteManager.sendQuote(message);
     }
   },
