5282f9808dbd2182cb132b29dbc7464fbb085567
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 23:19:11 +0530

    think.js

--- commands/emotes/think.js
+++ commands/emotes/think.js
@@ -1,12 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const THINK_MESSAGES = [
     "{author} is deep in thought...",
     "{author} is thinking hard.",
     "{author} contemplates the situation."
 ];
-
 module.exports = {
     name: 'think',
     description: 'Express thinking or pondering.',
@@ -14,12 +12,12 @@ module.exports = {
         const gifUrl = await fetchEmote('think');
         const randomMsg = THINK_MESSAGES[Math.floor(Math.random() * THINK_MESSAGES.length)]
             .replace("{author}", message.author);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
