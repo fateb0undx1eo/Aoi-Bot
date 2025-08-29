f8bfc18dd51e069d70e2bd117a3fa3d5d4119fed
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 23:15:48 +0530

    sleep.js

--- commands/emotes/sleep.js
+++ commands/emotes/sleep.js
@@ -1,12 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const SLEEP_MESSAGES = [
     "{author} falls asleep peacefully.",
     "{author} is taking a nap!",
     "{author} is drifting into dreamland..."
 ];
-
 module.exports = {
     name: 'sleep',
     description: 'Take a peaceful nap!',
@@ -14,12 +12,12 @@ module.exports = {
         const gifUrl = await fetchEmote('sleep');
         const randomMsg = SLEEP_MESSAGES[Math.floor(Math.random() * SLEEP_MESSAGES.length)]
             .replace("{author}", message.author);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
