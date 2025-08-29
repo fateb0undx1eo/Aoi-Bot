7325fcc70dccd26e09086a77180e177d41fcffef
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 20:59:45 +0530

    neko.js

--- commands/emotes/neko.js
+++ commands/emotes/neko.js
@@ -1,17 +1,16 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 module.exports = {
     name: 'neko',
     description: 'Get a random neko image!',
     async execute(message) {
         const gifUrl = await fetchEmote('neko');
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(`${message.author} found a cute neko!`)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
