db0be0252eae0c488ceb9c4732ea32b3e2383e95
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 20:18:06 +0530

    bored.js

--- commands/emotes/bored.js
+++ commands/emotes/bored.js
@@ -1,12 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const BORED_MESSAGES = [
     "{author} looks bored...",
     "{author} has nothing to do.",
     "{author} sighs in boredom."
 ];
-
 module.exports = {
     name: 'bored',
     description: 'Show that you are bored.',
@@ -14,12 +12,12 @@ module.exports = {
         const gifUrl = await fetchEmote('bored');
         const randomMsg = BORED_MESSAGES[Math.floor(Math.random() * BORED_MESSAGES.length)]
             .replace("{author}", message.author);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
