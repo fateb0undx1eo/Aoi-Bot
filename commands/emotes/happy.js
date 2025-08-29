6f3106e58c5352285963d03f3d8b92a1bf6ceb48
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 20:30:02 +0530

    happy.js

--- commands/emotes/happy.js
+++ commands/emotes/happy.js
@@ -1,12 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const HAPPY_MESSAGES = [
     "{author} is feeling really happy!",
     "{author} smiles brightly in joy!",
     "{author} is full of happiness!"
 ];
-
 module.exports = {
     name: 'happy',
     description: 'Express your happiness!',
@@ -14,12 +12,12 @@ module.exports = {
         const gifUrl = await fetchEmote('happy');
         const randomMsg = HAPPY_MESSAGES[Math.floor(Math.random() * HAPPY_MESSAGES.length)]
             .replace("{author}", message.author);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
