6ff0def6c6df515531a0e765e33da3a8d1c869fd
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 21:00:27 +0530

    nod.js

--- commands/emotes/nod.js
+++ commands/emotes/nod.js
@@ -1,12 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const NOD_MESSAGES = [
     "{author} nods in agreement.",
     "{author} gives a confirming nod.",
     "{author} silently agrees."
 ];
-
 module.exports = {
     name: 'nod',
     description: 'Nod in agreement.',
@@ -14,12 +12,12 @@ module.exports = {
         const gifUrl = await fetchEmote('nod');
         const randomMsg = NOD_MESSAGES[Math.floor(Math.random() * NOD_MESSAGES.length)]
             .replace("{author}", message.author);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
