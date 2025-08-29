50375f212917b18e7cf3497c8e9d8fd37f1463c7
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 23:18:26 +0530

    stare.js

--- commands/emotes/stare.js
+++ commands/emotes/stare.js
@@ -1,12 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const STARE_MESSAGES = [
     "{author} stares silently...",
     "{author} keeps a focused gaze.",
     "{author} looks intently."
 ];
-
 module.exports = {
     name: 'stare',
     description: 'Stare silently or with focus.',
@@ -14,12 +12,12 @@ module.exports = {
         const gifUrl = await fetchEmote('stare');
         const randomMsg = STARE_MESSAGES[Math.floor(Math.random() * STARE_MESSAGES.length)]
             .replace("{author}", message.author);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
