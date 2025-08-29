8e132d8e4e866e5cb45552a3a95e4d4c786936fb
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 20:16:59 +0530

    blush.js

--- commands/emotes/blush.js
+++ commands/emotes/blush.js
@@ -1,12 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const BLUSH_MESSAGES = [
     "{author} turns bright red!",
     "{author} blushes shyly.",
     "{author} can't hide their blush."
 ];
-
 module.exports = {
     name: 'blush',
     description: 'Blush adorably!',
@@ -14,12 +12,12 @@ module.exports = {
         const gifUrl = await fetchEmote('blush');
         const randomMsg = BLUSH_MESSAGES[Math.floor(Math.random() * BLUSH_MESSAGES.length)]
             .replace("{author}", message.author);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
