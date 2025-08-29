4a9f436fd2c888f98d8579ef20b31153ff79e53a
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-30 00:00:59 +0530

    wink.js

--- commands/emotes/wink.js
+++ commands/emotes/wink.js
@@ -1,12 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const WINK_MESSAGES = [
     "{author} winks playfully!",
     "{author} gives a cheeky wink.",
     "{author} winks with confidence!"
 ];
-
 module.exports = {
     name: 'wink',
     description: 'Send a playful wink.',
@@ -14,12 +12,12 @@ module.exports = {
         const gifUrl = await fetchEmote('wink');
         const randomMsg = WINK_MESSAGES[Math.floor(Math.random() * WINK_MESSAGES.length)]
             .replace("{author}", message.author);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
