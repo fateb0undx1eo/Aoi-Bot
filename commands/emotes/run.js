0e564f526ece089ff543658f8e9515035f6b17ea
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 21:08:52 +0530

    run.js

--- commands/emotes/run.js
+++ commands/emotes/run.js
@@ -1,12 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const RUN_MESSAGES = [
     "{author} starts running!",
     "{author} dashes away quickly!",
     "{author} runs off into the distance."
 ];
-
 module.exports = {
     name: 'run',
     description: 'Run away or just run!',
@@ -14,12 +12,12 @@ module.exports = {
         const gifUrl = await fetchEmote('run');
         const randomMsg = RUN_MESSAGES[Math.floor(Math.random() * RUN_MESSAGES.length)]
             .replace("{author}", message.author);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
