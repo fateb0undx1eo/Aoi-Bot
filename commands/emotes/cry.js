f1373753baff6d0f1eb15fba9ac505eeee256c7a
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 20:19:10 +0530

    cry.js

--- commands/emotes/cry.js
+++ commands/emotes/cry.js
@@ -1,12 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const CRY_MESSAGES = [
     "{author} starts crying...",
     "{author} tears up.",
     "{author} can't hold back the tears."
 ];
-
 module.exports = {
     name: 'cry',
     description: 'Cry your heart out.',
@@ -14,12 +12,12 @@ module.exports = {
         const gifUrl = await fetchEmote('cry');
         const randomMsg = CRY_MESSAGES[Math.floor(Math.random() * CRY_MESSAGES.length)]
             .replace("{author}", message.author);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
