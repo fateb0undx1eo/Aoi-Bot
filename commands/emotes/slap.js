fda4ae9fa7e721180fecf9dbb41341c81ef48ca3
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 23:14:49 +0530

    slap.js

--- commands/emotes/slap.js
+++ commands/emotes/slap.js
@@ -1,6 +1,5 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const SLAP_MESSAGES = [
     "{author} slaps {target} playfully! ",
     "{author} gives {target} a big slap! 😆",
@@ -8,36 +7,31 @@ const SLAP_MESSAGES = [
     "{author} sends a dramatic slap to {target}! ",
     "{author} teaches {target} a lesson with a slap! "
 ];
-
 module.exports = {
     name: 'slap',
     description: 'Slap someone playfully!',
-
     async execute(message) {
         const member = message.mentions.members.first();
-
         if (!member) {
             const msg = await message.reply("❌ Tag someone to slap!");
             setTimeout(() => msg.delete().catch(() => {}), 5000);
             return;
         }
-
         if (member.id === message.author.id) {
             const msg = await message.reply("😂 Slapping yourself? That’s bold, but try someone else!");
             setTimeout(() => msg.delete().catch(() => {}), 5000);
             return;
         }
-
         const gifUrl = await fetchEmote('slap');
         const randomMsg = SLAP_MESSAGES[Math.floor(Math.random() * SLAP_MESSAGES.length)]
             .replace("{author}", message.author)
             .replace("{target}", member);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
