50a2c74f43330da1a398140ba6d20c82c02ed326
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 20:35:51 +0530

    kitsune.js

--- commands/emotes/kitsune.js
+++ commands/emotes/kitsune.js
@@ -1,17 +1,16 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 module.exports = {
     name: 'kitsune',
     description: 'Get a random kitsune image!',
     async execute(message) {
         const gifUrl = await fetchEmote('kitsune');
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(`${message.author} summoned a mystical kitsune!`)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
