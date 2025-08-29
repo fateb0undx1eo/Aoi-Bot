3d343a81ce57f84525f466810f3de3a26429a3ac
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 20:58:39 +0530

    lurk.js

--- commands/emotes/lurk.js
+++ commands/emotes/lurk.js
@@ -1,12 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const LURK_MESSAGES = [
     "{author} is lurking silently...",
     "{author} hides in the shadows.",
     "{author} is watching quietly from afar."
 ];
-
 module.exports = {
     name: 'lurk',
     description: 'Lurk in the background.',
@@ -14,12 +12,12 @@ module.exports = {
         const gifUrl = await fetchEmote('lurk');
         const randomMsg = LURK_MESSAGES[Math.floor(Math.random() * LURK_MESSAGES.length)]
             .replace("{author}", message.author);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
