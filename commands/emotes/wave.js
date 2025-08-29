c70fb3d14496c629b405a81be1fb5ce244c73c04
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 23:56:13 +0530

    wave.js

--- commands/emotes/wave.js
+++ commands/emotes/wave.js
@@ -1,42 +1,36 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const WAVE_MESSAGES = [
     "{author} waves enthusiastically at {target}! ",
     "{author} gives a cheerful wave to {target}!",
     "{author} waves at {target}, catching their attention!",
     "{author} greets {target} with a friendly wave!"
 ];
-
 module.exports = {
     name: 'wave',
     description: 'Wave at someone!',
-
     async execute(message) {
         const member = message.mentions.members.first();
-
         // Handle no mention
         if (!member) {
             return message.reply("❌ Tag someone to wave at!")
                 .then(msg => setTimeout(() => msg.delete().catch(() => {}), 5000));
         }
-
         // Prevent self-wave
         if (member.id === message.author.id) {
             return message.reply("Waving at yourself in the mirror?")
                 .then(msg => setTimeout(() => msg.delete().catch(() => {}), 5000));
         }
-
         const gifUrl = await fetchEmote("wave");
         const randomMsg = WAVE_MESSAGES[Math.floor(Math.random() * WAVE_MESSAGES.length)]
             .replace("{author}", message.author)
             .replace("{target}", member);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
