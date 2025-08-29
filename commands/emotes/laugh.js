23f5f243a5281e21dacf3a5730701cfaf7bc039e
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 20:36:37 +0530

    laugh.js

--- commands/emotes/laugh.js
+++ commands/emotes/laugh.js
@@ -1,12 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const LAUGH_MESSAGES = [
     "{author} laughs out loud!",
     "{author} is laughing uncontrollably!",
     "{author} bursts into laughter!"
 ];
-
 module.exports = {
     name: 'laugh',
     description: 'Laugh out loud!',
@@ -14,12 +12,12 @@ module.exports = {
         const gifUrl = await fetchEmote('laugh');
         const randomMsg = LAUGH_MESSAGES[Math.floor(Math.random() * LAUGH_MESSAGES.length)]
             .replace("{author}", message.author);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
