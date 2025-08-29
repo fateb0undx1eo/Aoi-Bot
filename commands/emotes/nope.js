52eb183baf4768b33e4aca0740ebe765d316cdbf
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 21:02:17 +0530

    nope.js

--- commands/emotes/nope.js
+++ commands/emotes/nope.js
@@ -1,12 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const NOPE_MESSAGES = [
     "{author} shakes their head in disapproval.",
     "{author} says nope!",
     "{author} disagrees completely."
 ];
-
 module.exports = {
     name: 'nope',
     description: 'Express disapproval.',
@@ -14,12 +12,12 @@ module.exports = {
         const gifUrl = await fetchEmote('nope');
         const randomMsg = NOPE_MESSAGES[Math.floor(Math.random() * NOPE_MESSAGES.length)]
             .replace("{author}", message.author);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
