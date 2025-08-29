6cfd8c3ef9ce8aa92ba1f98aa6160a406ff554dc
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 23:20:00 +0530

    thumbsup.js

--- commands/emotes/thumbsup.js
+++ commands/emotes/thumbsup.js
@@ -1,12 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const THUMBSUP_MESSAGES = [
     "{author} gives a thumbs up!",
     "{author} approves with a thumbs up.",
     "{author} shows their support!"
 ];
-
 module.exports = {
     name: 'thumbsup',
     description: 'Give a thumbs up of approval.',
@@ -14,12 +12,12 @@ module.exports = {
         const gifUrl = await fetchEmote('thumbsup');
         const randomMsg = THUMBSUP_MESSAGES[Math.floor(Math.random() * THUMBSUP_MESSAGES.length)]
             .replace("{author}", message.author);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
