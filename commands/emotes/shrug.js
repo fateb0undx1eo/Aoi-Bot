feea0ec6b954076198002e83259560c0c9056630
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 23:12:20 +0530

    shrug.js

--- commands/emotes/shrug.js
+++ commands/emotes/shrug.js
@@ -1,12 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const SHRUG_MESSAGES = [
     "{author} shrugs casually.",
     "{author} doesn't know what to say.",
     "{author} shrugs it off."
 ];
-
 module.exports = {
     name: 'shrug',
     description: 'Express indifference or confusion.',
@@ -14,12 +12,12 @@ module.exports = {
         const gifUrl = await fetchEmote('shrug');
         const randomMsg = SHRUG_MESSAGES[Math.floor(Math.random() * SHRUG_MESSAGES.length)]
             .replace("{author}", message.author);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
