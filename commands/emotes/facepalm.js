be98433e0f707b7a20b264cdc09f06b67245805f
fateb0undx1eo <explainersenpai@gmail.com>
2025-08-29 20:22:34 +0530

    facepalm.js

--- commands/emotes/facepalm.js
+++ commands/emotes/facepalm.js
@@ -1,12 +1,10 @@
 const { EmbedBuilder } = require('discord.js');
 const { fetchEmote } = require('../../utils/emoteFetcher');
-
 const FACEPALM_MESSAGES = [
     "{author} facepalms.",
     "{author} can't believe what just happened.",
     "{author} covers their face in disappointment."
 ];
-
 module.exports = {
     name: 'facepalm',
     description: 'Show your disappointment.',
@@ -14,12 +12,12 @@ module.exports = {
         const gifUrl = await fetchEmote('facepalm');
         const randomMsg = FACEPALM_MESSAGES[Math.floor(Math.random() * FACEPALM_MESSAGES.length)]
             .replace("{author}", message.author);
-
         const embed = new EmbedBuilder()
             .setColor(Math.floor(Math.random() * 16777215))
             .setDescription(randomMsg)
             .setImage(gifUrl);
-
         await message.channel.send({ embeds: [embed] });
+        // Delete the user's command message to keep chat tidy
+        message.delete().catch(() => {});
     }
 };
